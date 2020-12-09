67dc7952783fa211701b98fdff2912d97020d13f
xindong <xin.dong@intel.com>
2020-12-09 11:49:46 +0800

    fix build error

--- core/src/main/java/org/apache/spark/util/collection/unsafe/sort/PMemSpillWriterFactory.java
+++ core/src/main/java/org/apache/spark/util/collection/unsafe/sort/PMemSpillWriterFactory.java
@@ -55,6 +55,7 @@ public class PMemSpillWriterFactory {
                 new PMemWriter(
                         externalSorter,
                         sortedSpillIte,
+                        numberOfRecordsToWritten,
                         writeMetrics,
                         taskMetrics);
             }
